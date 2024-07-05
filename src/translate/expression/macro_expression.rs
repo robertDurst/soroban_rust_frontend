d4817ae94dc35df01d5cdafd5eaef13c8ec9a515
Rob Durst <me@robdurst.com>
2024-07-04 19:52:02 -0600

    add the rest of the rust files

---
+++ src/translate/expression/macro_expression.rs
@@ -0,0 +1,18 @@
+use crate::common::compilation_state;
+use crate::common::handle_macro;
+use crate::instruction::Instruction;
+use crate::{
+    errors::not_translatable_error::NotTranslatableError, //, translate::expression::parse_expression,
+};
+use syn::ExprMacro;
+
+pub fn handle_macro_expression(
+    expr: &ExprMacro,
+    compilation_state: &mut compilation_state::CompilationState,
+) -> Result<Vec<Instruction>, NotTranslatableError> {
+    handle_macro(
+        &expr.mac,
+        compilation_state.next_assignment.clone(),
+        compilation_state,
+    )
+}
