b6558ffc65300870df940929f462660e397a1608
Rob Durst <me@robdurst.com>
2024-07-14 17:36:05 -0600

    handle break statement explicitly

--- src/translate/expression/break_expression.rs
+++ src/translate/expression/break_expression.rs
@@ -7,25 +7,14 @@ pub fn handle_break_expression(
     _expr: &ExprBreak,
     compilation_state: &mut compilation_state::CompilationState,
 ) -> Result<Vec<Instruction>, NotTranslatableError> {
-    let prev_scope = compilation_state.scope();
-
     let mut instructions_to_return = vec![];
-
-    let last_parent_scope = compilation_state.outside_last_loop_scope();
-
-    while compilation_state.scope() != last_parent_scope {
-        compilation_state.exit_scope();
-    }
-
-    // we want to exit the parent scope after all
-    compilation_state.exit_scope();
 
     instructions_to_return.push(Instruction::new(
         compilation_state.get_global_uuid(),
-        "jump".to_string(),
-        vec![compilation_state.scope().to_string()],
+        "break".to_string(),
+        vec![],
         "".to_string(),
-        prev_scope,
+        compilation_state.scope(),
     ));
 
     Ok(instructions_to_return)
@@ -47,10 +36,10 @@ mod tests {
             instructions,
             vec![Instruction::new(
                 1,
-                "jump".to_string(),
-                vec![compilation_state.scope().to_string()],
+                "break".to_string(),
+                vec![],
                 "".to_string(),
-                0,
+                compilation_state.scope(),
             ),]
         );
     }
