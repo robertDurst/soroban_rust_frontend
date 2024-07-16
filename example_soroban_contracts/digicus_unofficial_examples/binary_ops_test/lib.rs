8e7f84b034fc0f841e47364246e649f6c31f4588
Rob Durst <me@robdurst.com>
2024-07-15 22:10:05 -0600

    make sure jump after break

---
+++ example_soroban_contracts/digicus_unofficial_examples/binary_ops_test/lib.rs
@@ -0,0 +1,7 @@
+pub fn binary_ops_test() {
+    let x = 10;
+    x += 2;
+    x -= 3;
+    x *= 4;
+    x /= 5;
+}
--- src/tests.rs
+++ src/tests.rs
@@ -74,6 +74,7 @@ mod full_contract_tests {
     test_contract!(digicus_unofficial_examples, simple_sum_in_for_loop);
     test_contract!(digicus_unofficial_examples, return_error);
     test_contract!(digicus_unofficial_examples, match_on_numbers);
+    test_contract!(digicus_unofficial_examples, binary_ops_test);
     // Stellar Official Repo Examples
     test_contract!(stellar_official_repo_examples, account);
     test_contract!(stellar_official_repo_examples, alloc);
